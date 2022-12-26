5c81cff8005518968cfa8f12472239d4197241c5
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-26 15:58:55 +0200

    Add stayles to App.css

--- src/App.js
+++ src/App.js
@@ -6,38 +6,38 @@ function App() {
     <div className="App">
       <div className="container">
         <div id="display"></div>
-        <div>
+        <div className='buttons'>
           <div className="keypad">
             <div className='row'>
-              <div id="clear">AC</div>
-              <div id="delete"><Backspace/></div>
+              <div id="clear" className='clear button w2'>AC</div>
+              <div id="delete" className='clear button w1'><Backspace /></div>
             </div>
             <div className='row'>
-              <div id="seven">7</div>
-              <div id="eight">8</div>  
-              <div id="nine">9</div>
+              <div id="seven" className='button w1'>7</div>
+              <div id="eight" className='button w1'>8</div>  
+              <div id="nine" className='button w1'>9</div>
             </div>    
             <div className="row">
-              <div id="four">4</div>
-              <div id="five">5</div>
-              <div id="six">6</div>
+              <div id="four" className='button w1'>4</div>
+              <div id="five" className='button w1'>5</div>
+              <div id="six" className='button w1'>6</div>
             </div>    
             <div className="row">
-              <div id="one">1</div>
-              <div id="two">2</div>
-              <div id="three">3</div>
+              <div id="one" className='button w1'>1</div>
+              <div id="two" className='button w1'>2</div>
+              <div id="three" className='button w1'>3</div>
             </div> 
             <div className="row">
-              <div id="zero">0</div>
-              <div id="decimal">.</div>
+              <div id="zero" className='button w2'>0</div>
+              <div id="decimal" className='button w1'>.</div>
             </div>   
           </div>
-          <div className="operation">
-                <div id="equals"><Equal/></div>
-                <div id="divide"><Divide/></div>
-                <div id="multiply"><X/></div>
-                <div id="subtract"><Minus/></div>
-                <div id="add"><Plus/></div>
+          <div >
+                <div id="equals" className='operation button w1'><Equal /></div>
+                <div id="divide" className='operation button w1'><Divide/></div>
+                <div id="multiply" className='operation button w1'><X/></div>
+                <div id="subtract" className='operation button w1'><Minus/></div>
+                <div id="add" className='operation button w1'><Plus/></div>
           </div>
         </div>
 
